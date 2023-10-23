dc7379e9fcfc5602420a818d765813aa2eff746e
insert-user-here <143760337+insert-user-here@users.noreply.github.com>
2023-10-22 20:22:55 -0700

    Update uv.config.js

--- static/public/test/js/uv/uv.config.js
+++ static/public/test/js/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: './js/uv/uv.handler.js',
-    bundle: './js/uv/uv.bundle.js',
-    config: './js/uv/uv.config.js',
-    sw: './js/uv/uv.sw.js',
+    handler: '/test/js/uv/uv.handler.js',
+    bundle: '/test/js/uv/uv.bundle.js',
+    config: '/test/js/uv/uv.config.js',
+    sw: '/test/js/uv/uv.sw.js',
 };
